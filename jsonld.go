cdbaf2bf582a35c3a53cbc74fe67c9e9edb07885
Olivier Tremblay <olivier@is-a-dev.com>
2015-03-07 10:24:02 -0500

    Basic json-ld mapping

---
+++ jsonld.go
@@ -0,0 +1,8 @@
+package main
+
+type jsonld struct {
+	DataTypes  map[string]interface{} `json:datatypes`
+	Properties map[string]interface{} `json:properties`
+	Types      map[string]interface{} `json:types`
+	Valid      string                 `json:valid`
+}
