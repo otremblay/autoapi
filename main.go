044c5921b0db9e33449db0b3fa414e4044d8a198
Robert Melton <robert@robertmelton.com>
2015-05-25 13:25:37 -0500

    Just made main a little more friendly with flags
    Signed-off-by: Olivier Tremblay <olivier@is-a-dev.com>

--- main.go
+++ main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"database/sql"
+	"flag"
 	"fmt"
+	"log"
 	"os"
 	"strings"
 
@@ -12,19 +14,64 @@ import (
 	_ "github.com/ziutek/mymysql/godrv"
 )
 
-func main() {
-	dbHost := os.Args[1]
-	dbName := os.Args[2]
-	dbUname := os.Args[3]
+const (
+	// Ugly way to check to see if they passed in a password
+	// chance of collision with a GUID is very low
+	defaultPassValue = "5e7dc6f6a1a94c39be95b88a47c2458b"
+)
 
-	fmt.Print("Password:")
-	pass := strings.TrimSpace(string(gopass.GetPasswdMasked()))
-	db, err := sql.Open("mymysql", fmt.Sprintf("tcp:%s:3306*%s/%s/%s", dbHost, dbName, dbUname, pass))
+var (
+	dbPort  string
+	dbHost  string
+	dbName  string
+	dbUname string
+	dbPass  string
+)
+
+func init() {
+	flag.StringVar(&dbPort, "P", "3306", "port")
+	flag.StringVar(&dbPass, "p", defaultPassValue, "password")
+	flag.StringVar(&dbHost, "h", "localhost", "host")
+	flag.StringVar(&dbName, "d", "", "database name")
+	flag.StringVar(&dbUname, "u", "root", "username")
+	flag.Parse()
+}
+
+func main() {
+	pass := dbPass
+	if pass == defaultPassValue {
+		fmt.Print("Password:")
+		pass = strings.TrimSpace(string(gopass.GetPasswdMasked()))
+	}
+
+	if strings.TrimSpace(dbPort) == "" {
+		fmt.Println("Missing port")
+		flag.PrintDefaults()
+		os.Exit(1)
+	}
+	if strings.TrimSpace(dbHost) == "" {
+		fmt.Println("Missing host")
+		flag.PrintDefaults()
+		os.Exit(1)
+	}
+	if strings.TrimSpace(dbName) == "" {
+		fmt.Println("Missing database name")
+		flag.PrintDefaults()
+		os.Exit(1)
+	}
+	if strings.TrimSpace(dbUname) == "" {
+		fmt.Println("Missing username")
+		flag.PrintDefaults()
+		os.Exit(1)
+	}
+
+	db, err := sql.Open("mymysql", fmt.Sprintf("tcp:%s:%s*%s/%s/%s", dbHost, dbPort, dbName, dbUname, pass))
 	if err != nil {
-		panic(err)
+		flag.PrintDefaults()
+		log.Panic(err)
 	}
 	err = lib.Generate(db, dbName)
 	if err != nil {
-		panic(err)
+		log.Panic(err)
 	}
 }
